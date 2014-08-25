e0124579c16e14a15c20e69e2afdfb05d4436bff
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-08-25 16:15:09 -0300

    just fixing a few spaces and classpath issues

--- src/controllers/FirstWeb.java
+++ src/controllers/FirstWeb.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class FirstWeb extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet (HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
         writer.println("<h1>Hello world servlet 2!</h1>");
     }
